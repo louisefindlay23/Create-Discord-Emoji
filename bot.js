5a66b3449339831087f9d56cd2437e23c2decea2
Louise Findlay <louise@louisefindlay.com>
2022-09-26 13:38:47 +0100

    Added if statement for if emoji name already exists

--- bot.js
+++ bot.js
@@ -16,20 +16,28 @@ client.on("interactionCreate", async (interaction) => {
     if (commandName === "emojibot") {
         const guild = interaction.guild;
         const emojiName = interaction.options.getString("name");
-        let emoji = interaction.options.getAttachment("emoji").attachment;
-        guild.emojis
-            .create({
-                attachment: emoji,
-                name: emojiName,
-            })
-            .then((emoji) => {
-                console.log(`Created new emoji with name ${emoji.name}`);
-                emoji = client.emojis.cache.find(
-                    (emoji) => emoji.name === emojiName
-                );
-                interaction.reply(`You created ${emojiName} emoji: ${emoji}`);
-            })
-            .catch(console.error);
+        if (client.emojis.cache.find((emoji) => emoji.name === emojiName)) {
+            interaction.reply(
+                "This emoji name already exists. Please run `/emojibot` again and pick a new name"
+            );
+        } else {
+            let emoji = interaction.options.getAttachment("emoji").attachment;
+            guild.emojis
+                .create({
+                    attachment: emoji,
+                    name: emojiName,
+                })
+                .then((emoji) => {
+                    console.log(`Created new emoji with name ${emoji.name}`);
+                    emoji = client.emojis.cache.find(
+                        (emoji) => emoji.name === emojiName
+                    );
+                    interaction.reply(
+                        `You created ${emojiName} emoji: ${emoji}`
+                    );
+                })
+                .catch(console.error);
+        }
     }
 });
 
