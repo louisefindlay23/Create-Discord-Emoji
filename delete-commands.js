71bc75a60bd21bacd6b06d05424b10c5df2e8320
Louise Findlay <louise@louisefindlay.com>
2022-09-26 13:57:41 +0100

    Changed from guild command to global command

--- delete-commands.js
+++ delete-commands.js
@@ -3,12 +3,6 @@ const { REST, Routes } = require("discord.js");
 
 const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 const clientID = process.env.CLIENT_ID;
-const guildID = process.env.GUILD_ID;
-
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: [] })
-    .then(() => console.log("Successfully deleted all guild commands."))
-    .catch(console.error);
 
 // for global commands
 rest.put(Routes.applicationCommands(clientID), { body: [] })
--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,6 @@ const { REST, SlashCommandBuilder, Routes } = require("discord.js");
 
 const token = process.env.BOT_TOKEN;
 const clientID = process.env.CLIENT_ID;
-const guildID = process.env.GUILD_ID;
 
 const commands = [
     new SlashCommandBuilder()
@@ -25,10 +24,16 @@ const commands = [
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: commands })
-    .then((data) =>
-        console.log(
-            `Successfully registered ${data.length} application commands.`
-        )
-    )
-    .catch(console.error);
+(async () => {
+    try {
+        console.log("Started refreshing application (/) commands.");
+
+        await rest.put(Routes.applicationCommands(clientID), {
+            body: commands,
+        });
+
+        console.log("Successfully reloaded application (/) commands.");
+    } catch (error) {
+        console.error(error);
+    }
+})();
